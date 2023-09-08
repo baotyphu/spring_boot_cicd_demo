88a65000958bad9458023e2aee77227acedd25aa
baotyphu <nguyenvanbao0704@gmail.com>
2023-09-09 06:19:48 +0700

    first commit

---
+++ src/main/java/com/example/demo1/resource/PersonsController.java
@@ -0,0 +1,20 @@
+package com.example.demo1.resource;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/persons")
+public class PersonsController {
+
+
+
+    @GetMapping("/get-all")
+    public String getAll() throws InterruptedException {
+        return "hello";
+    }
+
+
+}
