af6c9449c8fb240e8aafa7478fd736d123e5eb77
baotyphu <nguyenvanbao0704@gmail.com>
2023-09-10 17:08:48 +0700

    change nodePort

--- src/main/java/com/example/demo1/resource/PersonsController.java
+++ src/main/java/com/example/demo1/resource/PersonsController.java
@@ -18,7 +18,7 @@ public class PersonsController {
 
     @GetMapping("/test")
     public String getHello() throws InterruptedException {
-        return "welcome devops training688777";
+        return "welcome devops training6887773333333";
     }
 
 }
