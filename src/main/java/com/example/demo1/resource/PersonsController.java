72734a1dba1b715143e29e6eb016e43259ed8c30
baotyphu <nguyenvanbao0704@gmail.com>
2023-09-10 09:30:18 +0700

    change test API

--- src/main/java/com/example/demo1/resource/PersonsController.java
+++ src/main/java/com/example/demo1/resource/PersonsController.java
@@ -18,7 +18,7 @@ public class PersonsController {
 
     @GetMapping("/test")
     public String getHello() throws InterruptedException {
-        return "welcome devops training5";
+        return "welcome devops training6";
     }
 
 }
