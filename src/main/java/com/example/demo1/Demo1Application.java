23c02c79a650a27c0812e2f3ff32a43f224845dc
Nguyen Van Bao <nguyenvanbao@Nguyens-MacBook-Pro.local>
2025-04-09 21:10:23 -0700

    test

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -2,7 +2,7 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// comment
 @SpringBootApplication
 public class Demo1Application {
 
