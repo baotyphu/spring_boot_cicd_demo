6e6a7194c0897d78cb852d3c02e92f7978f2acaa
baotyphu <57627016+baotyphu@users.noreply.github.com>
2025-12-14 10:31:26 +0700

    Update return value in getAll method

--- src/main/java/com/example/demo1/resource/PersonsController.java
+++ src/main/java/com/example/demo1/resource/PersonsController.java
@@ -13,7 +13,7 @@ public class PersonsController {
     // API get all
     @GetMapping("/get-all")
     public String getAll() throws InterruptedException {
-        return "feature 3";
+        return "feature ******";
     }
 
     @GetMapping("/test")
