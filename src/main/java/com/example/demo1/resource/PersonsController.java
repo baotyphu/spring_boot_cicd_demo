804dfc407f88d0b66b4eb7241687938ffc1a9f90
baotyphu <57627016+baotyphu@users.noreply.github.com>
2025-12-14 10:56:32 +0700

    Update response message in getAll method

--- src/main/java/com/example/demo1/resource/PersonsController.java
+++ src/main/java/com/example/demo1/resource/PersonsController.java
@@ -13,7 +13,7 @@ public class PersonsController {
     // API get all
     @GetMapping("/get-all")
     public String getAll() throws InterruptedException {
-        return "feature 5";
+        return "feature remote";
     }
 
     @GetMapping("/test")
